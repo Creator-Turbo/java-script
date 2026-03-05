1ea886b8fa5f15141407de8744eeda9850e20ab1
Creator-Turbo <bablupandey446@gmail.com>
2026-03-05 10:48:23 +0530

    add new methods

--- basic/05_strings.js
+++ basic/05_strings.js
@@ -10,7 +10,7 @@ console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
 //======================================================================
 
-const gameName = new String('hiteshhhc');
+const gameName = new String('hite-shhhc');
 
 console.log(gameName[0]);
 console.log(gameName.__proto__);
@@ -18,5 +18,33 @@ console.log(gameName.__proto__);
 console.log(gameName.length);
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
+
+
+// ===================================================
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+
+const anotherString = gameName.slice(0,4);
+
+console.log(anotherString);
+
+// ============================================================
+
+
+const newStringOne = "   bablu     ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "hhtps://bablu.com/bablu%20pandey";
+
+console.log(url.replace('%20','-'));;
+
+console.log(url.includes('shishir'));
+
+
+
 
 
