94126d0e0968777a1d7bb1c1d8f532ebdc112cd3
Creator-Turbo <bablupandey446@gmail.com>
2026-03-13 17:49:43 +0530

    function 2

---
+++ 03basic/01_functions.js
@@ -0,0 +1,23 @@
+function sayMyName(){
+   console.log("b")
+   console.log("a")
+   console.log("b")
+   console.log("l")
+   console.log("u") 
+}
+
+// sayMyName()
+// sayMyName()
+// sayMyName()
+// sayMyName()
+// sayMyName()
+
+
+console.log("------------------------------------------------------------")
+
+function addTwoNumbers(number1 , number2){
+    console.log(number1 + number2)
+
+}
+
+addTwoNumbers(3,5)
---
+++ 03basic/02_functions.js
@@ -0,0 +1,27 @@
+function calculateCartPrice(val1,val2 ,...num1){
+    return num1
+}
+
+
+// console.log(calculateCartPrice(200,400,500,400,800,500000))
+
+
+
+const user = {
+    username : "bablu",
+    prices : 199
+}
+
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.prices}`)
+
+} 
+
+
+// handleObject(user)
+
+handleObject({
+    username : "bablu",
+    prices : 3940
+})
